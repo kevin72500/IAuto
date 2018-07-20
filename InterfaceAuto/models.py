fadaa747c88f1b31ce1ef091d5aeb8ba728845d0
kevin72500 <kevin72500@qq.com>
2018-07-20 20:05:43 +0800

    add new

--- InterfaceAuto/models.py
+++ InterfaceAuto/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class OrgInfo(models.Model):
-    org_id=models.IntegerField(auto_created=True)
-    org_name=models.CharField(max_length=20)
-    org_parent_id=models.IntegerField()
+    orgId=models.IntegerField(auto_created=True)
+    orgName=models.CharField(max_length=20)
+    orgParent_id=models.IntegerField()
     def __str__(self):
-        return self.org_name
+        return self.orgName
 
 
 class UserInfo(models.Model):
@@ -21,9 +21,31 @@ class UserInfo(models.Model):
         return self.name
 
 class ProjectInfo(models.Model):
+    id = models.IntegerField(auto_created=True, primary_key=True)
     pro_name=models.CharField(max_length=50)
     pro_des=models.CharField(max_length=200)
     def __str__(self):
         return self.pro_name
 
+
+class resultInfo(models.Model):
+    id = models.IntegerField(auto_created=True, primary_key=True)
+    desc=models.CharField(max_length=50)
+    res = models.CharField(max_length=50)
+    retCode = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    url = models.CharField(max_length=50)
+    retContent = models.CharField(max_length=10000)
+    def __str__(self):
+        return self.desc
+
+
+class resultMapInfo(models.Model):
+    id=models.IntegerField(auto_created=True, primary_key=True)
+    orgId = models.ForeignKey(OrgInfo, on_delete=models.CASCADE)
+    proId = models.ForeignKey(ProjectInfo, on_delete=models.CASCADE)
+    userId = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+    ressultId=models.ForeignKey(resultInfo, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.id
 
